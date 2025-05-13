179f929588e27d58272deb032779d60b3587bbbb
mfrizqi <muhammadfajarrizqi@gmail.com>
2025-05-13 15:21:12 +0800

    feat: wip chat room

--- src/components/chat/card.vue
+++ src/components/chat/card.vue
@@ -1,12 +1,52 @@
+<script setup>
+defineProps({
+  room: {},
+});
+
+const getInitialName = (name)=>{
+  return name[0]
+}
+
+</script>
+
 <template>
-  <section>
-    <div class="flex gap-4 text-xs">
-      <div>109220-Naturalization</div>
-      <div>January 1,2021 19:10</div>
+  <section
+    class="flex gap-4 text-sm border-b border-gray-500 pb-5.5 pt-6 first:pt-0 relative cursor-pointer"
+  >
+    <div class="relative" v-if="room.isConvo">
+      <div
+        class="h-[34px] w-[34px] rounded-full bg-[#2F80ED] relative left-[6px] z-10 flex justify-center items-center"
+      >
+        <img src="../../assets//img//icon/user.svg" class="w-[20px] h-[20px]" />
+      </div>
+      <div
+        class="h-[34px] w-[34px] rounded-full bg-[#E0E0E0] absolute top-0 left-[-10px] flex justify-center items-center"
+      >
+        <img
+          src="../../assets//img//icon/person_grey.svg"
+          class="w-[20px] h-[20px] grayscale"
+        />
+      </div>
     </div>
+    <div v-if="!room.isConvo">
+      <div
+        class="h-[34px] w-[34px] rounded-full bg-[#2F80ED] relative z-10 flex justify-center items-center"
+      >
+        <div class="text-white">{{ getInitialName(room.name) }}</div>
+      </div>
+    </div>
+    <div>
+      <div class="flex gap-4">
+        <div class="font-bold text-[#2F80ED]">{{ room.title }}</div>
+        <div class="grow shrink-0">{{ room.date }}</div>
+      </div>
+      <section>
+        <div class="font-bold text-sm">{{ room.name }} :</div>
+        <div class="line-clamp-1">{{ room.description }}</div>
+      </section>
+    </div>
+    <div v-if="room.isNew" class="absolute bg-[#EB5757] h-[10px] w-[10px] rounded-full right-0 bottom-[22px]"></div>
   </section>
 </template>
-<script>
-export default {};
-</script>
+
 <style lang=""></style>
--- src/components/chat/container.vue
+++ src/components/chat/container.vue
@@ -1,38 +1,95 @@
 <script setup>
+import { reactive } from "vue";
+
 import Loading from "../utils/Loading.vue";
-import ChatCard from "./card.vue"
+import ChatListCard from "./card.vue";
+import { uuid } from "vue-uuid";
+
+const chat = reactive({
+  room: [
+    {
+      id: uuid.v4(),
+      title: "109220-Naturalization",
+      date: "January 1,2021 19:10",
+      name: "Cameron Phillips",
+      description: "Please check this out!",
+      isNew: true,
+      isConvo: true,
+    },
+    {
+      id: uuid.v4(),
+      title:
+        "Jeannette Moraima Guaman Chamba (Hutto I-589) [ Hutto Follow Up - Brief Service ]",
+      date: "02/06/2021 10:45",
+      name: "Ellen",
+      description: "Hey, please read.",
+      isNew: false,
+      isConvo: true,
+    },
+    {
+      id: uuid.v4(),
+      title: "8405-Diana SALAZAR MUNGUIA",
+      date: "01/06/2021 12:19",
+      name: "Cameron Phillips",
+      description:
+        "I understand your initial concerns and thats very valid, Elizabeth. But you need to look over from the company perspective",
+      isNew: false,
+      isConvo: true,
+    },
+    {
+      id: uuid.v4(),
+      title: "FastVisa Support",
+      date: "01/06/2021 12:19",
+      name: "FastVisa",
+      description: "Hey there! Welcome to your inbox.",
+      isNew: false,
+      isConvo: false,
+    },
+  ],
+  state: 'list'
+});
+
+const selectChatroom = (id)=>{
+  chat.state = id
+}
+
 </script>
 
 <template>
   <section>
-    <div class="flex">
-      <!-- <Loading title="Loading Chats..."></Loading> -->
-      <div class="input w-full px-8 mx-6 mt-5 bg-white border border-gray-400 text-gray-800 h-[32px]">
-        <input type="search" class="grow" placeholder="Search" />
-        <svg
-          class="h-[1em] opacity-50"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
+    <section v-if="chat.state === 'list'">
+      <div class="flex sticky top-0 bg-white z-20 pb-5.5">
+        <!-- <Loading title="Loading Chats..."></Loading> -->
+        <div
+          class="input w-full px-8 mx-6 mt-6 bg-white border border-gray-400 text-gray-800 h-[32px]"
         >
-          <g
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            stroke-width="2.5"
-            fill="none"
-            stroke="#1e2939"
+          <input type="search" class="grow" placeholder="Search" />
+          <svg
+            class="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
           >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </g>
-        </svg>
+            <g
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke-width="2.5"
+              fill="none"
+              stroke="#1e2939"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+        </div>
       </div>
-    </div>
-    <section class="px-8 pt-5.5">
-      <ChatCard/>
+      <div class="px-8">
+        <ChatListCard v-for="room in chat.room" :key="room.id" :room="room" @click="selectChatroom(room.id)" />
+      </div>
+    </section>
+    <section v-if="chat.state !== 'list'" class="px-8 py-5">
+      Chatroom
     </section>
   </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
